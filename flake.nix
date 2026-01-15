5c3573ecc20d884a254e852664936d9f2d4a2bea
Emile Clark-Boman <emile@agribit.com.au>
2026-01-15 12:48:48 +1000

    systems input is unnecessary

--- flake.nix
+++ flake.nix
@@ -1,10 +1,5 @@
 {
   description = "Mix - (Better) Modules For Nix";
 
-  inputs.systems.url = "github:nix-systems/default";
-
-  outputs = {...} @ inputs: let
-    systems = import inputs.systems;
-  in
-    import ./mix {inherit systems;};
+  outputs = {...}: import ./mix;
 }
