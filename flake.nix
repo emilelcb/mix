b5d4dcdd1fba1270b20ea7ad749ae3084fcc2539
Emile Clark-Boman <emile@agribit.com.au>
2026-01-15 13:14:40 +1000

    add missing input "nib"
    currently using a local flake for debugging

--- flake.nix
+++ flake.nix
@@ -1,5 +1,12 @@
 {
   description = "Mix - (Better) Modules For Nix";
 
-  outputs = {...}: import ./mix {};
+  inputs = {
+    nib = {
+      # url = "github:emilelcb/nib";
+      url = "/home/me/agribit/nexus/nib";
+    };
+  };
+
+  outputs = inputs: import ./mix inputs;
 }
